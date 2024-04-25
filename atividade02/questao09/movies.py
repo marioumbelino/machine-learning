6b057911ae65f6c2039c10c9f91b30d1911d02ca
marioumbelino <marioherbert2016@gmail.com>
2024-04-25 20:10:56 -0300

    Questão 09.2
    Corrige problema de diretório do dataset

--- atividade02/questao09/movies.py
+++ atividade02/questao09/movies.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-movies_df = pd.read_csv('atividade02/questao08/movies.csv')
+movies_df = pd.read_csv('atividade02/questao09/movies.csv')
 
 most_popular = movies_df[(movies_df["Popularity"] > 1000) & (movies_df["Vote_average"] > 8)].reset_index()
 
