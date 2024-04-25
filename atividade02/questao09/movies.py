e41aee16892a023efa93b0f619c0d8fb8973865a
marioumbelino <marioherbert2016@gmail.com>
2024-04-25 19:50:55 -0300

    Questão 09

---
+++ atividade02/questao09/movies.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+movies_df = pd.read_csv('atividade02/questao08/movies.csv')
+
+most_popular = movies_df[(movies_df["Popularity"] > 1000) & (movies_df["Vote_average"] > 8)].reset_index()
+
+print(most_popular["Name"])
